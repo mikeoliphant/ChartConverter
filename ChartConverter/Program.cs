6f40ab1eaa3716993e1036eeb90983bc4d32d137
Mike Oliphant <oliphant@nostatic.org>
2025-01-01 08:44:58 -0800

    Use DX for windows version

--- ChartConverter/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UILayout;
-using ChartConverter;
-
-using var host = new ChartConverterHost(1024, 720, isFullscreen: false);
-
-MonoGameLayout layout = new MonoGameLayout();
-
-host.StartGame(layout);
---
+++ ChartConverterShared/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UILayout;
+using ChartConverter;
+
+namespace ChartPlayer
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using var host = new ChartConverterHost(1024, 720, isFullscreen: false);
+
+            MonoGameLayout layout = new MonoGameLayout();
+
+            host.StartGame(layout);
+        }
+    }
+}
